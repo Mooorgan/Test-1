20005bcec0896849aac0731485f4aa96a90a5508
ShaishavPaudel <morganhouse15@gmail.com>
2022-11-02 13:42:54 +0545

    place has been added

--- newFile.js
+++ newFile.js
@@ -4,3 +4,4 @@ const name2 = "hello my nameis shaishav paudel";
 const name3 = "I live in Kusunti near Ekantakuna ";
 const food = "I like momo";
 const pet = "I like dogs";
+const palce = "I want ot go to pokhara";
