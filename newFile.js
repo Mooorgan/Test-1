f58a5ca26f1f5a3a4ddedd13e3623bdea352f294
ShaishavPaudel <morganhouse15@gmail.com>
2022-11-02 13:38:37 +0545

    food added

--- newFile.js
+++ newFile.js
@@ -2,3 +2,4 @@ const a = "hello Javascript!!!!";
 const name1 = "hey!!!";
 const name2 = "hello my nameis shaishav paudel";
 const name3 = "I live in Kusunti near Ekantakuna ";
+const food = "I like momo";
